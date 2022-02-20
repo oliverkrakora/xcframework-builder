5e60121d646a9e83f55a03f5df4672dd8b660731
Oliver Ian Krakora <Oliverkrakora@gmail.com>
2022-02-20 08:42:40 -0600

    replaced debug flag with verbose flag
    - added new flag for ignoring nested frameworks
    - restructured code

--- Sources/xcframework-builder/XCFrameworkBuilder.swift
+++ Sources/xcframework-builder/XCFrameworkBuilder.swift
@@ -15,12 +15,11 @@ struct XCFrameworkBuilder: ParsableCommand {
     }
     
     /// If set prints out debugging messages and keeps the working directory where the conversion takes place
-    @Flag(name: .customLong("debug"))
-    private var isDebug: Bool = false
+    @Flag(name: .customLong("verbose"))
+    private var isVerbose: Bool = false
     
-    /// If set deletes the original framework that was passed as the input
     @Flag
-    private var deleteInputFileOnSuccess: Bool = false
+    private var ignoreNestedFrameworks: Bool = false
     
     @Option(name: .customLong("framework-input-path"))
     private var fileInputURL: URL
@@ -33,59 +32,75 @@ struct XCFrameworkBuilder: ParsableCommand {
     }
     
     func createXCFramework(for originalFrameworkURL: URL) throws {
-        try createXCFramework(for: originalFrameworkURL, outputURL: outputURL, deleteInputFileOnSuccess: deleteInputFileOnSuccess, isDebug: isDebug)
+        try createXCFramework(for: originalFrameworkURL, outputURL: outputURL, isVerbose: isVerbose)
     }
         
-    func createXCFramework(for originalFrameworkURL: URL, outputURL: URL, deleteInputFileOnSuccess: Bool, isDebug: Bool) throws {
+    func createXCFramework(for originalFrameworkURL: URL, outputURL: URL, isVerbose: Bool) throws {
         
-        guard !fileInputURL.pathExtension.isEmpty else {
+        // make sure that the input url points to a framework
+        guard fileInputURL.pathExtension == "framework" else {
             throw Error.invalidPath
         }
         
+        // make sure the output url points to a directory
         guard outputURL.pathExtension.isEmpty else {
             throw Error.invalidPath
         }
-                
+        
+        // make sure lipo is installed
         guard Bash.isCommandInstalled(Lipo.commandName) else {
             throw Error.commandNotInstalled(commandName: Lipo.commandName)
         }
         
+        // make sure xcodebuild is installed
         guard Bash.isCommandInstalled(Xcodebuild.commandName) else {
             throw Error.commandNotInstalled(commandName: Xcodebuild.commandName)
         }
         
-        //create working dir
+        // create working dir
         let workingDir = URL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent(UUID().uuidString)
         try FileManager.default.createDirectory(at: workingDir, withIntermediateDirectories: true, attributes: nil)
         
-        if isDebug {
-            print("working dir is located at: \(workingDir.path)")
-        }
+        log("working dir is located at: \(workingDir.path)")
         
         // An url of the framework located in the workingDir
         let frameworkURL = workingDir.appendingPathComponent(originalFrameworkURL.lastPathComponent)
         
-        // the file name of the framework bundle
-        let frameworkName = originalFrameworkURL.deletingPathExtension().lastPathComponent
-        
-        let frameworkOutputURL = workingDir.appendingPathComponent(frameworkName).appendingPathExtension("xcframework")
-        
         // copy framework from originalFrameworkURL to working dir
         try FileManager.default.copyItem(atPath: originalFrameworkURL.path, toPath: frameworkURL.path)
         
-        // the frameworks folder inside the framework
-        let nestedFrameworksURL = frameworkURL.appendingPathComponent("Frameworks")
+        try processNestedBinaries(at: frameworkURL, outputDir: workingDir)
         
-        if FileManager.default.fileExists(atPath: nestedFrameworksURL.path, isDirectory: nil) {
-            let frameworks = try FileManager.default.contentsOfDirectory(at: nestedFrameworksURL, includingPropertiesForKeys: [.isDirectoryKey], options: [.skipsPackageDescendants, .skipsSubdirectoryDescendants]).filter { $0.pathExtension == "framework" }
-            
-            for frameworkURL in frameworks {
-                try createXCFramework(for: frameworkURL, outputURL: workingDir, deleteInputFileOnSuccess: false, isDebug: false)
-            }
-            
-            try FileManager.default.removeItem(at: nestedFrameworksURL)
+        try processBinary(at: frameworkURL, outputDir: workingDir)
+                        
+        // Make sure output directory exists
+        try FileManager.default.createDirectory(at: outputURL, withIntermediateDirectories: true, attributes: nil)
+        
+        // Moving xcframeworks to output directory
+        
+        log("Moving result to output directory")
+        
+        let xcframeworkURLs = try FileManager.default.contentsOfDirectory(at: workingDir, includingPropertiesForKeys: [.isDirectoryKey], options: [.skipsSubdirectoryDescendants, .skipsPackageDescendants]).filter { $0.pathExtension == "xcframework" }
+
+        for xcframeworkURL in xcframeworkURLs {
+            try FileManager.default.moveItem(at: xcframeworkURL, to: outputURL.appendingPathComponent(xcframeworkURL.lastPathComponent))
         }
-                
+        
+        // clean up
+        try FileManager.default.removeItem(at: workingDir)
+    }
+    
+    /// Processes the Mach-O fat binary located inside the framework
+    /// - Parameter frameworkURL: A url pointing to the framework that should be processed
+    /// - Parameter outputDir: A url pointing to the output directory where the processed framework should be moved
+    ///
+    /// The Mach-O fat binary is sliced into smaller binaries containing platform specific architectures
+    private func processBinary(at frameworkURL: URL, outputDir: URL) throws {
+        // the file name of the framework bundle
+        let frameworkName = frameworkURL.deletingPathExtension().lastPathComponent
+        
+        let frameworkOutputURL = outputDir.appendingPathComponent(frameworkName).appendingPathExtension("xcframework")
+
         let lipo = Lipo(frameworkURL: frameworkURL)
         
         let architectures = try lipo.availableArchitectures()
@@ -94,11 +109,13 @@ struct XCFrameworkBuilder: ParsableCommand {
         var frameworkOutputURLs = [URL]()
         
         for platform in architecturesByPlatform{
+            log("Processing binary of framework")
             // create folder with name of the platform that will contain the converted framework
-            var frameworkCopyURL = workingDir.appendingPathComponent(platform.key.rawValue)
+            var frameworkCopyURL = outputDir.appendingPathComponent(platform.key.rawValue)
             try FileManager.default.createDirectory(at: frameworkCopyURL, withIntermediateDirectories: true, attributes: nil)
-            frameworkCopyURL.appendPathComponent(frameworkName)
-            frameworkCopyURL.appendPathExtension("framework")
+            frameworkCopyURL = frameworkCopyURL
+                .appendingPathComponent(frameworkName)
+                .appendingPathExtension("framework")
             frameworkOutputURLs.append(frameworkCopyURL)
             
             // make copy of original input framework and place it in the architecture folder
@@ -106,31 +123,40 @@ struct XCFrameworkBuilder: ParsableCommand {
             // remove the fat Mach-O binary from the framework copy
             try FileManager.default.removeItem(at: frameworkCopyURL.appendingPathComponent(frameworkName))
             
+            log("Extracting platform \(platform.key.rawValue) from framework")
             // extract all platform architectures from original framework
             try lipo.extractArchitectures(platform.value, to: frameworkCopyURL.appendingPathComponent(frameworkName).path)
         }
         
         try Xcodebuild.createXCFramework(from: frameworkOutputURLs, outputURL: frameworkOutputURL)
+    }
+    
+    /// Processes the frameworks located in the Frameworks folder of the given `frameworkURL`
+    ///
+    /// - Parameter frameworkURL: The url pointing to the framework that should be processed
+    /// - Parameter outputDir: A url pointing to a directory where the processed frameworks should be moved
+    /// The folder is not processed if there is none or if the `ignoreNestedFrameworks` flag is set
+    private func processNestedBinaries(at frameworkURL: URL, outputDir: URL) throws {
+        // the frameworks folder inside the framework
+        let nestedFrameworksURL = frameworkURL.appendingPathComponent("Frameworks")
+        guard FileManager.default.fileExists(atPath: nestedFrameworksURL.path, isDirectory: nil) && !ignoreNestedFrameworks else { return }
         
-        // Make sure output directory exists
-        try FileManager.default.createDirectory(at: outputURL, withIntermediateDirectories: true, attributes: nil)
+        log("Processing nested frameworks of \(frameworkURL.lastPathComponent)")
+        let frameworks = try FileManager.default.contentsOfDirectory(at: nestedFrameworksURL, includingPropertiesForKeys: [.isDirectoryKey], options: [.skipsPackageDescendants, .skipsSubdirectoryDescendants]).filter { $0.pathExtension == "framework" }
         
-        // Moving xcframeworks to output directory
+        for frameworkURL in frameworks {
+            log("Processing \(frameworkURL.lastPathComponent)")
+            try createXCFramework(for: frameworkURL, outputURL: outputDir, isVerbose: false)
+        }
         
-        let xcframeworkURLs = try FileManager.default.contentsOfDirectory(at: workingDir, includingPropertiesForKeys: [.isDirectoryKey], options: [.skipsSubdirectoryDescendants, .skipsPackageDescendants]).filter { $0.pathExtension == "xcframework" }
+        try FileManager.default.removeItem(at: nestedFrameworksURL)
+    }
+}
 
-        for xcframeworkURL in xcframeworkURLs {
-            try FileManager.default.moveItem(at: xcframeworkURL, to: outputURL.appendingPathComponent(xcframeworkURL.lastPathComponent))
-        }
-        
-        // cleanup
-        
-        if !isDebug {
-            try FileManager.default.removeItem(at: workingDir)
-        }
-        
-        if deleteInputFileOnSuccess {
-            try FileManager.default.removeItem(at: originalFrameworkURL)
-        }
+// MARK: Helpers
+extension XCFrameworkBuilder {
+    private func log(_ message: String) {
+        guard isVerbose else { return }
+        print(message)
     }
 }
